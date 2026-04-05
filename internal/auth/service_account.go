bff18bd5d5346d4d065eec7a566749c35a93e511
iamrils <amrilsyaifa10@gmail.com>
2026-04-05 21:03:39 +0700

    fix: golangci

--- internal/auth/service_account.go
+++ internal/auth/service_account.go
@@ -1,10 +1,13 @@
 package auth
 
 import (
+	"context"
 	"fmt"
 	"os"
 
+	"golang.org/x/oauth2/google"
 	"google.golang.org/api/option"
+	"google.golang.org/api/sheets/v4"
 )
 
 func ServiceAccountOption(credentialsPath string) (option.ClientOption, error) {
@@ -12,5 +15,9 @@ func ServiceAccountOption(credentialsPath string) (option.ClientOption, error) {
 	if err != nil {
 		return nil, fmt.Errorf("unable to read credentials file: %w", err)
 	}
-	return option.WithCredentialsJSON(data), nil
+	cfg, err := google.JWTConfigFromJSON(data, sheets.SpreadsheetsScope)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse service account key: %w", err)
+	}
+	return option.WithTokenSource(cfg.TokenSource(context.Background())), nil
 }
